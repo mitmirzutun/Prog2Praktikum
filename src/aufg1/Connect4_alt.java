528faae0f845bd4e258acc65e00eb026d73f64dc
mitmirzutun <milan1111@gmx.de>
2020-03-20 22:24:44 +0100

    Reworked Connect4_alt

--- src/aufg1/Connect4_alt.java
+++ src/aufg1/Connect4_alt.java
@@ -1,8 +1,8 @@
 package aufg1;
 import java.io.IOException;
 public class Connect4_alt {
-    private char[][] pos=new char[8][8];
-    private boolean player1=true;
+    private char pos[][]=new char[8][8],empty='.',player1='X',player2='O';
+    private boolean player1Turn=true;
     private boolean gameEnded=false;
     public static void main(String[] args) throws IOException {
         Connect4_alt m = new Connect4_alt();
@@ -11,23 +11,33 @@ public class Connect4_alt {
     public void start() throws IOException {
         for (int i=0;i<8;i++)
             for (int j=0;j<8;j++)
-                pos[i][j]='.';
+                pos[i][j]=empty;
+        gameEnded=false;
         while (!gameEnded)
             this.game();
     }
     private void game() throws IOException {
         display();
+        if (gameEnded)
+        	return;
         char c = (char)System.in.read();
         while(c=='\n'||c=='\r') {
             c = (char)System.in.read();
         }
         if (!('1'<=c&&c<='8')) {
             if (c=='q'||c=='Q') {
+            	c=(char)System.in.read();
+            	if (c!='\n'&&c!='\r') {
+            		System.out.println("Invalid Input");
+            		do {
+            			c = (char)System.in.read();
+            		} while (c!='\n'||c!='\r');
+            	}
                 System.out.println("Quitting...");
                 gameEnded=true;
                 return;
             }
-            System.out.println("Invalide Eingabe");
+            System.out.println("Invalid Input");
             do {
                 c=(char)System.in.read();
             } while(c!='\n'&&c!='\r');
@@ -36,55 +46,87 @@ public class Connect4_alt {
         {
             char c_end = (char)System.in.read();
             if (c_end!='\n'&&c_end!='\r') {
-                System.out.println("Invalide Länge");
+                System.out.println("Invalid length");
                 do {
                     c_end=(char)System.in.read();
                 } while(c_end!='\n'&&c_end!='\r');
                 return;
             }
         }
-        if (pos[0][c-'1']!='.') {
+        if (pos[0][c-'1']!=empty) {
             System.out.println("Reihe bereits voll");
             return;
         }
         for (int i =7;i>=0;i--) 
-            if (pos[i][c-'1']=='.') {
-                pos[i][c-'1']=player1?'O':'X';
+            if (pos[i][c-'1']==empty) {
+                pos[i][c-'1']=player1Turn?player1:player2;
                 break;
             }
-        this.winner();
-        player1=!player1;
+        player1Turn=!player1Turn;
     }
     private void display() {
-        for (int i=1;i<=8;i++)
-            System.out.print(i+" ");
-        System.out.println();
-        for (int i=0;i<8;i++) {
-            for (int j=0;j<8;j++)
-                System.out.print(pos[i][j]+" ");
-            System.out.println();
-        }
+        System.out.print(this.toString());
     }
-    private void winner() {
+    public String toString() {
+    	if (gameEnded)
+    		return winner();
+    	StringBuilder sb= new StringBuilder(197);
+    	for (int i=1;i<=8;i++) {
+    		sb.append(i);
+    		sb.append(' ');
+    	}
+    	sb.append('\n');
+    	for (int i=0;i<8;i++) {
+    		for (int j=0;j<8;j++) {
+    			sb.append(pos[i][j]);
+    			sb.append(' ');
+    		}
+    		sb.append('\n');
+    	}
+    	for (int i=1;i<=8;i++) {
+    		sb.append(i);
+    		sb.append(' ');
+    	}
+    	String tmp=winner();
+    	sb.append('\n');
+    	if (gameEnded) {
+    		sb.append(tmp);
+    		return sb.toString();
+    	}
+    	sb.append("Turn of Player ");
+    	sb.append(player1Turn?player1:player2);
+    	sb.append('\n');
+    	return sb.toString();
+    }
+    private String winner() {
+    	String pl1won,pl2won;
+    	{
+    		StringBuilder tmp = new StringBuilder(16);
+    		tmp.append("Player ");
+    		tmp.append(player1);
+    		tmp.append(" won");
+    		pl1won=tmp.toString();
+    		tmp = new StringBuilder(16);
+    		tmp.append("Player ");
+    		tmp.append(player2);
+    		tmp.append(" won");
+    		pl2won=tmp.toString();
+    	}
         for (int i=0;i<8;i++) {
             for (int j=0;j<5;j++) {
                 if (pos[i][j]==pos[i][j+1]&&
                         pos[i][j]==pos[i][j+2]&&
                         pos[i][j]==pos[i][j+3]&&
-                        pos[i][j]!='.') {
-                    this.display();
-                    System.out.println((player1?"Player 1":"Player 2")+" won");
+                        pos[i][j]!=empty) {
                     gameEnded=true;
-                    return;
+                    return pos[i][j]==player1?pl1won:pl2won;
                 }
                 if (pos[j][i]==pos[j+1][i]&&
                         pos[j][i]==pos[j+2][i]&&
                         pos[j][i]==pos[j+3][i]&&
                         pos[j][i]!='.') {
-                    this.display();
-                    System.out.println((player1?"Player 1":"Player 2")+" won");
                     gameEnded=true;
-                    return;
+                    return pos[j][i]==player1?pl1won:pl2won;
                 }
             }
         }
@@ -94,21 +136,18 @@ public class Connect4_alt {
                         pos[i][j]==pos[i+2][j+2]&&
                         pos[i][j]==pos[i+3][j+3]&&
                         pos[i][j]!='.') {
-                    this.display();
-                    System.out.println((player1?"Player 1":"Player 2")+" won");
                     gameEnded=true;
-                    return;
+                    return pos[j][i]==player1?pl1won:pl2won;
                 }
                 if (pos[i+3][j]==pos[i+2][j+1]&&
                         pos[i+3][j]==pos[i+1][j+2]&&
                         pos[i+3][j]==pos[i][j+3]&&
                         pos[i+3][j]!='.') {
-                    this.display();
-                    System.out.println((player1?"Player 1":"Player 2")+" won");
                     gameEnded=true;
-                    return;
+                    return pos[j][i+3]==player1?pl1won:pl2won;
                 }
             }
         }
+        return "Tie!";
     }
 }
