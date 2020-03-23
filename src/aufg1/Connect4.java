89a11d783e433a7e855ed3c37479a078606d5dde
mitmirzutun <milan1111@gmx.de>
2020-03-23 18:01:23 +0100

    Updated Connect4

--- src/aufg1/Connect4.java
+++ src/aufg1/Connect4.java
@@ -1,167 +1,170 @@
 package aufg1;
-public class Connect4{
-    private static boolean[][][] data = new boolean[8][8][2];
-    //data[i][j][0] = true wenn token in Position vorhanden ist
-    //data[i][j][1] = true wenn token von Player 1 ist
-    //data[i][j][1] = false wenn token von Player 2 ist
-    public static void main(String[] args) throws Exception {
-        draw();
-        while(true){
-            player(1);
-            draw();
-            if(gameEnded()){
-		    	System.out.println("Player 1 hat gewonnen");
-		    	break;
-	    	}
-            player(2);
-            draw();
-            if(gameEnded()){
-		    	System.out.println("Player 2 hat gewonnen");
-		    	break;
-	    	}
-        }
-    }
-    private static void draw(){
-        for (int a=7;a>=0  ;a-- ) {//Zeilen von 8 bis 0
-            for (int b=0;b<=7  ;b++ ) {//Spalten von 0 bis 8
-                if (data[a][b][0]){
-                    if (data[a][b][1]) {
-						System.out.print("[X]");
-					}
-                    else{
-						System.out.print("[O]");
-					}
-                }
-                else{
-					System.out.print("[ ]");
-				}
-            }
-            System.out.println();
-        }
-    }
-    private static void player(int player)throws Exception{
-        System.out.println("Player "+ player +" ist am Zug");
-		System.out.println("Wähle Spalte 1-8");
-		int input = System.in.read();
-		while(System.in.read()!='\n'){}
-		input -= 49;
+import java.util.HashMap;
 
-		for (int a=7;a>=0  ;a-- ) {//Zeilen von 8 bis 0
-			if (data[a][input][0] == true){
-				data[a+1][input][0] = true;
-				if (player == 1){data[a+1][input][1] = true;}
-				else{data[a+1][input][1] = false;}
+public class Connect4 {
+	public static void main(String args[]) throws Exception {
+		System.out.println(1>=0);
+		Connect4 c4=new Connect4();
+		c4.start();
+	}
+	public void start() throws Exception {
+		boolean player1Turn=true;
+		Field f=new Field();
+		while (true) {
+			System.out.println(f.toString());
+			if (f.winner().containsKey(true)) {
+				System.out.print("Player ");
+				System.out.print(f.winner().containsValue('X')?'X':'O');
+				System.out.println(" has won");
 				return;
 			}
+			if (f.isFull()) {
+				System.out.println("Tie!");
+				return;
+			}
+			System.out.print("Turn of Player ");
+			System.out.println(player1Turn?"O":"X");
+			char c1=(char)System.in.read(),c2=(char)System.in.read();
+			while (c1=='\n'||c1=='\r') {
+				c1=c2;
+				c2=(char)System.in.read();
+			}
+			if (c2!='\n'&&c2!='\r'||c1<'1'||c1>'8') {
+				System.out.println("invalid input");
+				continue;
+			}
+			if (f.isFull(c1-'1')) {
+				System.out.println("Row full");
+				continue;
+			}
+			f.insertToken(c1-'1', player1Turn?'O':'X');
+			player1Turn=!player1Turn;
 		}
-		data[0][input][0] = true;
-		if (player == 1){data[0][input][1] = true;}
-		else{data[0][input][1] = false;}
-    }
-    
-    /**
-    * Returns if the game has ended
-    */
-    public static boolean gameEnded() {
-        if (!rowsEmpty())
-        	return true;
-        if (winner()==0)
-        	return false;
-        return true;
-    }
-    /**
-    * Returns the Winner of the game.
-    */
-    public static byte winner() {
-        for (int i=0;i<data.length;i++) {
-        	for (int j=0;j<data.length&&data[i][j+3][0];j++) {
-        		if (data[i][j][1]==data[i][j+1][1]&&
-        			data[i][j][1]==data[i][j+2][1]&&
-        			data[i][j][1]==data[i][j+3][1]) {
-        			return (byte)(data[i][j][1]?2:1);
-        		}
-        	}
-        }
-        for (int i=0;i+3<data.length;i++) {
-        	for (int j=0;j<data.length&&data[i][j][0]&&data[i+1][j][0]
-        			&&data[i+2][j][0]&&data[i+3][j][0];j++) {
-        		if (data[i][j][1]==data[i+1][j][1]&&
-            		data[i][j][1]==data[i+2][j][1]&&
-            		data[i][j][1]==data[i+3][j][1]) {
-        			return (byte)(data[i][j][1]?2:1);
-        		}
-        	}
-        }
-        for (int i=0;i+3<data.length;i++) {
-        	for (int j=0;j<data.length&&data[i][j][0]&&data[i+1][j+1][0]
-        			&&data[i+2][j+2][0]&&data[i+3][j+3][0];j++) {
-        		if (data[i][j][1]==data[i+1][j+1][1]&&
-                	data[i][j][1]==data[i+2][j+2][1]&&
-                	data[i][j][1]==data[i+3][j+3][1]) {
-            		return (byte)(data[i][j][1]?2:1);
-            	}
-        	}
-        	for (int j=0;j<data.length&&data[i][j+3][0]&&data[i+1][j+2][0]
-        			&&data[i+2][j+1][0]&&data[i+3][j][0];j++) {
-        		if (data[i][j+3][1]==data[i+1][j+2][1]&&
-                	data[i][j+3][1]==data[i+2][j+1][1]&&
-                	data[i][j+3][1]==data[i+3][j][1]) {
-            		return (byte)(data[i][j+3][1]?2:1);
-            	}
-        	}
-        }
-        return (byte)0;
-    }
-    private static boolean rowsEmpty() {
-
-        for (int i=0;i<data.length;i++)
-        	if (!data[i][7][0])
-        		return true;
-        return false;
-    }
+	}
 }
 class Field {
-	private Vector[] field= new Vector[8];
+	private Vector[] field;
 	public Field() {
-		for (int i=0;i<field.length;i++)
+		field=new Vector[8];
+		for (int i=0;i<field.length;i++) {
 			field[i]=new Vector();
+		}
+	}
+	public void insertToken(int line, char token) {
+		field[line].addToken(token);
+	}
+	public boolean isFull() {
+		for (int i=0;i<8;i++)
+			if (!this.isFull(i))
+				return false;
+		return true;
+	}
+	public boolean isFull(int i) {
+		return field[i].getLength()==8;
+	}
+	public HashMap<Boolean, Character> winner() {
+		HashMap<Boolean,Character> tmp= new HashMap<Boolean, Character>();
+		for (int i=0;i<8;i++) {
+			for (int j=0;j<field[i].getLength()-3;j++) {
+				if (field[i].getToken(j)==field[i].getToken(j+1)&&
+						field[i].getToken(j)==field[i].getToken(j+2)&&
+						field[i].getToken(j)==field[i].getToken(j+3)) {
+					tmp.put(true, field[i].getToken(j));
+					return tmp;
+				}
+			}
+		}
+		for (int i=0;i<5;i++) {
+			for (int j=0;j<field[i].getLength()&&
+					j<field[i+1].getLength()&&
+					j<field[i+2].getLength()&&
+					j<field[i+3].getLength();j++) {
+				if (field[i].getToken(j)==field[i+1].getToken(j)&&
+						field[i+1].getToken(j)==field[i+2].getToken(j)&&
+						field[i].getToken(j)==field[i+3].getToken(j)) {
+					tmp.put(true, field[i].getToken(j));
+					return tmp;
+				}
+			}
+			for (int j=0;j<field[i].getLength()&&
+					j<field[i+1].getLength()-1&&
+					j<field[i+2].getLength()-2&&
+					j<field[i+3].getLength()-3;j++) {
+				if (field[i].getToken(j)==field[i+1].getToken(j+1)&&
+						field[i+1].getToken(j+1)==field[i+2].getToken(j+2)&&
+						field[i].getToken(j)==field[i+3].getToken(j+3)) {
+					tmp.put(true, field[i].getToken(j));
+					return tmp;
+				}
+			}
+			for (int j=0;j<field[i].getLength()-3&&
+					j<field[i+1].getLength()-2&&
+					j<field[i+2].getLength()-1&&
+					j<field[i+3].getLength();j++) {
+				if (field[i].getToken(j+3)==field[i+1].getToken(j+2)&&
+						field[i+1].getToken(j+2)==field[i+2].getToken(j+1)&&
+						field[i].getToken(j+3)==field[i+3].getToken(j)) {
+					tmp.put(true, field[i].getToken(j+3));
+					return tmp;
+				}
+			}
+		}
+		tmp.put(false, (char)0);
+		return tmp;
+	}
+	public String toString() {
+		StringBuilder sb = new StringBuilder(200);
+		int[] tmp=new int[8];
+		for (int i=0;i<8;i++) {
+			sb.append(i+1);
+			sb.append(' ');
+			tmp[i]=field[i].getLength();
+		}
+		sb.append('\n');
+		for (int i=0;i<=7;i++) {
+			for (int j=0;j<8;j++) {
+				sb.append((tmp[j]<=7-i?'.':field[j].getToken(7-i)));
+				sb.append(' ');
+			}
+			sb.append('\n');
+		}
+		for (int i=1;i<=8;i++) {
+			sb.append(i);
+			sb.append(' ');
+		}
+		return sb.toString();
 	}
 }
 class Vector {
-	private char tokens[];
+	private int n=0;
+	private char[] tokens;
 	public Vector() {
-		tokens=new char[0];
+		tokens=new char[8];
 	}
-	public Vector(Vector v) {
-		tokens=v.tokens.clone();
-	}
-	public Vector newToken(char token) {
-		if (tokens.length==8||won())
+	public void addToken(char token) {
+		if (n==tokens.length)
 			throw new ArrayIndexOutOfBoundsException();
-		Vector v=new Vector();
-		v.tokens=new char[tokens.length+1];
-		for (int i=0;i<tokens.length;i++)
-			v.tokens[i]=tokens[i];
-		v.tokens[tokens.length]=token;
-		return v;
+		tokens[n++]=token;
 	}
-	public int length() {
-		return tokens.length;
+	public char getToken(int index) {
+		if (index>=n)
+			throw new ArrayIndexOutOfBoundsException(index);
+		return tokens[index];
 	}
-	public char[] getTokens() {
-		return tokens.clone();
+	public int getLength() {
+		return n;
 	}
-	public char winner() {
-		for (int i=0;i+3<tokens.length;i++) {
-			if (tokens[i]==tokens[i+1]&&tokens[i+2]==tokens[i+3]&&tokens[i+1]==tokens[i+2])
-				return tokens[i];
-		} return 0;
-	}
-	public boolean won() {
-		for (int i=0;i+3<tokens.length;i++) {
-			if (tokens[i]==tokens[i+1]&&tokens[i+2]==tokens[i+3]&&tokens[i+1]==tokens[i+2])
-				return true;
-		} return false;
-		
+	public HashMap<Boolean,Character> winner() {
+		HashMap<Boolean,Character> tmp = new HashMap<Boolean, Character>();
+		for (int i=0;i<n-3;i++) {
+			if (tokens[i]==tokens[i+1]&&
+					tokens[i]==tokens[i+2]&&
+					tokens[i]==tokens[i+3]) {
+				tmp.put(true, tokens[i]);
+				return tmp;
+			}
+		}
+		tmp.put(false,(char)0);
+		return tmp;
 	}
 }
